843ce31afbc8494c2a4150dd19e09a1dfa6a5e7e
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-10-10 21:40:28 +0600

    All the component strcuture is created. Now I will work with state and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import Header from './Components/Header'
-import './App.css'
-
+import { useState } from "react";
+import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import TransactionList from "./Components/TransactionList";
+import AddTransaction from "./Components/AddTransaction";
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,8 +12,14 @@ function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
