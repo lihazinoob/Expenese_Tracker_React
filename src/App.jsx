aa747d17fc2ca8e4ef8219fafc586ba2360bbf60
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-10-10 21:21:19 +0600

    Header Component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import Header from './Components/Header'
+import './App.css'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>Hello World</div>
+      <Header />
     </>
   )
 }
